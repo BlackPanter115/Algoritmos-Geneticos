540c9808deb9bf9bbaf95c37b94637e5b162db52
BlackPanter115 <miguel.rojas115@hotmail.com>
2017-09-29 15:26:47 -0500

    Archivo Main

---
+++ Practica2/Grafica/Principal.java
@@ -0,0 +1,11 @@
+package grafica;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Grafica plot = new Grafica();
+        plot.generarGrafica();
+    }
+    
+}
